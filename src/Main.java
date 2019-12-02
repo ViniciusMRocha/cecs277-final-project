33440b6b56676d831e0aa876c588dfc0190168ec
TylerM <tyler.mcdonald1105@gmail.com>
2019-12-02 15:17:37 -0800

    Hallo

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Cory Reardon");
         System.out.println("Vini Rocha");
+        System.out.println("Tyler McDonald");
     }
 }
